cfbf4c415c992993af56cd5f34de26470fe9b3b2
CCYEE <ccyee4@gmail.com>
2023-06-21 23:47:46 -0700

    proj 12

---
+++ app.js
@@ -0,0 +1,23 @@
+// PORT
+const { PORT = 3001 } = process.env;
+
+// Connect Express
+const express = require("express");
+
+const app = express();
+
+// mongoose connection
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
+
+// Routes
+const routes = require("./routes");
+
+app.use(express.json());
+app.use(routes);
+
+// Listener
+app.listen(PORT, () => {
+  console.log(`App is listening on port: ${PORT}`);
+});
