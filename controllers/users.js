c0873dee82ea0e6fc12ad59ee4fa5991d6c22beb
CCYEE <ccyee4@gmail.com>
2023-06-21 23:54:05 -0700

    corrections to proj 12

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,7 @@
+const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+
 
 const { itemError } = require("../utils/errors");
 const { JWT_SECRET } = require("../utils/config");
