a9095f00e7e092763bf131bc280423056663bc6f
CCYEE <ccyee4@gmail.com>
2023-06-27 21:14:13 -0700

    schema errors

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 
-const userSchema = new mongoose.Schema({
+const userSchema = mongoose.Schema({
   name: {
     type: String,
     minlength: 2,
