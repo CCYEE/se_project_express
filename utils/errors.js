5d0f6cd29e00734f56710dff8845a5b6dc1759ae
CCYEE <ccyee4@gmail.com>
2023-07-03 19:28:55 -0700

    updates

--- utils/errors.js
+++ utils/errors.js
@@ -4,9 +4,7 @@ const ERROR_500 = 500;
 
 const itemError = (req, res, e) => {
   if (e.name === "ValidationError") {
-    return res
-      .status(ERROR_400)
-      .send({ message: "Invalid Data has been entered" });
+    return res.status(ERROR_400).send({ message: "Invalid Data entered" });
   }
   if (e.name === "CastError") {
     return res.status(ERROR_400).send({ message: "Invalid ID" });
@@ -14,7 +12,7 @@ const itemError = (req, res, e) => {
   if (e.name === "DocumentNotFoundError") {
     return res.status(ERROR_404).send({ message: "Error: Not Found" });
   }
-  return res.status(ERROR_500).send({ message: "Server error has occured" });
+  return res.status(ERROR_500).send({ message: "Server error occured" });
 };
 
 module.exports = {
